e2c64796af076f9cb484d9f070d56bacb4d95b58
muhadiyaksa <muhadiyaksa@gmail.com>
2022-10-13 23:22:10 +0700

    update fitur

--- src/pages/Chats.js
+++ src/pages/Chats.js
@@ -12,6 +12,7 @@ import IconProcess from "../assets/image/process2.png";
 import ModalElement from "../element/ModalElement";
 import Signup from "../assets/image/Signup.svg";
 import Sorry from "../assets/image/sorry.svg";
+import Disconnected from "../assets/image/disconnected.png";
 
 export default function Chats() {
   const socket = io.connect("http://localhost:3001");
@@ -40,6 +41,8 @@ export default function Chats() {
   const [dataProfilFriend, setDataProfilFriend] = useState({});
   const [showFriend, setShowFriend] = useState(false);
   const [spinnerFriend, setSpinnerFriend] = useState(false);
+  const [isNotAnggota, setIsNotAnggota] = useState(false);
+
   useEffect(() => {
     const getDataUser = () => {
       Axios({
@@ -62,14 +65,32 @@ export default function Chats() {
         headers: {
           Authorization: `Bearer ${userObj.token}`,
         },
-      }).then((result) => {
-        if (result.data) {
-          socket.emit("join_room", result.data.idroom);
-          socket.emit("data_user", userData._id);
-          setdataMasuk(result.data);
-          setIsLoading(false);
-        }
-      });
+      })
+        .then((result) => {
+          if (result.data) {
+            socket.emit("join_room", result.data.idroom);
+            socket.emit("data_user", userData._id);
+            socket.emit("cek_anggota", result.data.idroom);
+            setdataMasuk(result.data);
+            setIsLoading(false);
+            let dataAnggota = result.data.anggota.map((el) => el.iduser);
+            if (!dataAnggota.includes(userData._id)) {
+              setIsNotAnggota(true);
+              console.log("kamu gamasuk room nih");
+            } else {
+              setIsNotAnggota(false);
+              console.log("kamu masih jadi anggota kok");
+            }
+          }
+        })
+        .catch((err) => {
+          if (err.response.status === 404) {
+            setIsLoading(false);
+            setIsKeluar(true);
+          } else {
+            setIsLoading(false);
+          }
+        });
     };
     getDataRoom();
   }, []);
@@ -123,13 +144,25 @@ export default function Chats() {
       }
     });
 
+    socket.on("anggota_update", (data) => {
+      let dataAnggotaUpdate = data.map((el) => el.iduser);
+      console.log(data);
+      if (!dataAnggotaUpdate.includes(userData._id)) {
+        setIsKeluar(true);
+      } else {
+        setIsKeluar(false);
+      }
+    });
+
     socket.on("data_anggota", (data) => {
       setdataMasuk(data);
       // setIsLoading(false);
       if (data.anggota.length === 1) {
         setShow(false);
+        setIsSisaAnggota(false);
       } else {
         setShow(true);
+        setIsSisaAnggota(true);
       }
     });
   }, [socket]);
@@ -222,6 +255,31 @@ export default function Chats() {
       },
     }).then(() => {
       navigate("/find");
+    });
+  };
+
+  const joinRoom = () => {
+    Axios({
+      method: "POST",
+      withCredentials: true,
+      data: {
+        iduser: userData._id,
+        fotoUser: "",
+        namauser: dataUser.nama,
+        usernameuser: dataUser.username ? dataUser.username : "@anonymous",
+      },
+      url: `http://localhost:3001/rejoinchats/${param.idroom}`,
+      headers: {
+        Authorization: `Bearer ${userObj.token}`,
+      },
+    }).then((res) => {
+      if (res.data.status === "finish") {
+        socket.emit("data_anggota", res.data.idroom);
+        socket.emit("anggota_masuk", { idroom: res.data.idroom, iduser: userData._id });
+        setIsNotAnggota(false);
+        setIsLoading(false);
+        setIsKeluar(false);
+      }
     });
   };
   const tampilTeman = () => {
@@ -366,84 +424,102 @@ export default function Chats() {
               </div>
             ) : (
               <>
-                {isKeluar === false ? (
+                {isNotAnggota === true ? (
+                  <div className="sorry mx-auto text-center pt-5">
+                    <img src={Disconnected} alt="Icon Disconnected" />
+                    <p className="mt-4">
+                      Ups Masalah Jaringan, Kamu keluar dari room <br />
+                      Join lagi kuy. . .
+                    </p>
+                    <Button isPrimary onClick={joinRoom} className="text-decoration-none text-white w-auto">
+                      Join Kembali
+                    </Button>
+                    <Button type="link" href="/find" className="text-secondary mt-4 d-block" style={{ fontSize: "12px" }}>
+                      Keluar
+                    </Button>
+                  </div>
+                ) : (
                   <>
-                    <div className="box">
-                      <div className="position-relative h-100">
-                        <div className="row header-chats align-items-center">
-                          <div className="col d-flex align-items-center">
-                            <span className="red"></span>
-                            <span>Live Sedang Berlangsung</span>
-                          </div>
-                          <div className="col text-end">
-                            <button className="btn btn-outline-danger me-3 me-sm-0" onClick={keluarRoom}>
-                              Keluar Room
-                            </button>
-                            <button className="btn btn-outline-cyan d-sm-none" onClick={geserList}>
-                              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-person-lines-fill text-cyan" viewBox="0 0 16 16">
-                                <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
-                              </svg>
-                            </button>
-                          </div>
-                        </div>
-                        <div className="list-chats">
-                          <div className="position-relative h-100 w-100 ">
-                            <div className="chat-value ">{tampilPesan()}</div>
-                          </div>
-                        </div>
-                        <div className="input-chats">
-                          <div className="position-relative">
-                            <textarea className="form-control input-pesan" placeholder="ketikan pesanmu disini" onChange={(e) => setPesanKirim(e.target.value)} />
-                            <Button className="btn shadow-none" onClick={sendMessage}>
-                              <img src={ChatSend} alt="Send" />
-                            </Button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="friends">
-                      <div className="position-relative h-100">
-                        <div className="header-friends">
-                          <div className="d-flex justify-content-between align-items-center">
-                            <p className="judul-1 ">Topik Live Sesi</p>
-                            <button className="btn p-0 d-sm-none" onClick={tutupList}>
-                              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-x-lg text-dark" viewBox="0 0 16 16">
-                                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
-                              </svg>
-                            </button>
-                          </div>
-                          <div className=" d-flex flex-column flex-lg-row align-items-center justify-content-center">
-                            <img src={dataMasuk?.kategori === "mm" ? mm : dataMasuk?.kategori === "jarkom" ? jarkom : rpl} alt="" className="img-fluid topik me-3" />
-                            <p className="judul-1 text-cyan text-center text-lg-start tema">
-                              Tema : <br /> {dataMasuk?.kategori === "mm" ? "Multimedia" : dataMasuk?.kategori === "jarkom" ? "Jaringan Komputer" : "Rekayasa Perangkat Lunak"}
-                            </p>
-                          </div>
-                        </div>
-                        <div className="list-friends">
-                          <p className="judul-1 mb-2">Daftar Anggota</p>
-                          <div className="items-friends">
-                            <div className="item-friends" key={userData.iduser}>
-                              <div className="d-flex align-items-center">
-                                <div className="image me-lg-3 me-1">
-                                  <img src="" alt="" />
-                                </div>
-                                <div className="friend">
-                                  <p className="p-0 m-0">Saya</p>
-                                  <Button type="link" href="/">
-                                    {userData.username}
-                                  </Button>
-                                </div>
+                    {isKeluar === false ? (
+                      <>
+                        <div className="box">
+                          <div className="position-relative h-100">
+                            <div className="row header-chats align-items-center">
+                              <div className="col d-flex align-items-center">
+                                <span className="red"></span>
+                                <span>Live Sedang Berlangsung</span>
+                              </div>
+                              <div className="col text-end">
+                                <button className="btn btn-outline-danger me-3 me-sm-0" onClick={keluarRoom}>
+                                  Keluar Room
+                                </button>
+                                <button className="btn btn-outline-cyan d-sm-none" onClick={geserList}>
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-person-lines-fill text-cyan" viewBox="0 0 16 16">
+                                    <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
+                                  </svg>
+                                </button>
                               </div>
                             </div>
-                            {tampilTeman()}
+                            <div className="list-chats">
+                              <div className="position-relative h-100 w-100 ">
+                                <div className="chat-value ">{tampilPesan()}</div>
+                              </div>
+                            </div>
+                            <div className="input-chats">
+                              <div className="position-relative">
+                                <textarea className="form-control input-pesan" placeholder="ketikan pesanmu disini" onChange={(e) => setPesanKirim(e.target.value)} />
+                                <Button className="btn shadow-none" onClick={sendMessage}>
+                                  <img src={ChatSend} alt="Send" />
+                                </Button>
+                              </div>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </div>
+
+                        <div className="friends">
+                          <div className="position-relative h-100">
+                            <div className="header-friends">
+                              <div className="d-flex justify-content-between align-items-center">
+                                <p className="judul-1 ">Topik Live Sesi</p>
+                                <button className="btn p-0 d-sm-none" onClick={tutupList}>
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-x-lg text-dark" viewBox="0 0 16 16">
+                                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                                  </svg>
+                                </button>
+                              </div>
+                              <div className=" d-flex flex-column flex-lg-row align-items-center justify-content-center">
+                                <img src={dataMasuk?.kategori === "mm" ? mm : dataMasuk?.kategori === "jarkom" ? jarkom : rpl} alt="" className="img-fluid topik me-3" />
+                                <p className="judul-1 text-cyan text-center text-lg-start tema">
+                                  Tema : <br /> {dataMasuk?.kategori === "mm" ? "Multimedia" : dataMasuk?.kategori === "jarkom" ? "Jaringan Komputer" : "Rekayasa Perangkat Lunak"}
+                                </p>
+                              </div>
+                            </div>
+                            <div className="list-friends">
+                              <p className="judul-1 mb-2">Daftar Anggota</p>
+                              <div className="items-friends">
+                                <div className="item-friends" key={userData.iduser}>
+                                  <div className="d-flex align-items-center">
+                                    <div className="image me-lg-3 me-1">
+                                      <img src="" alt="" />
+                                    </div>
+                                    <div className="friend">
+                                      <p className="p-0 m-0">Saya</p>
+                                      <Button type="link" href="/">
+                                        {userData.username}
+                                      </Button>
+                                    </div>
+                                  </div>
+                                </div>
+                                {tampilTeman()}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </>
+                    ) : (
+                      <Navigate to="/find" />
+                    )}
                   </>
-                ) : (
-                  <Navigate to="/find" />
                 )}
               </>
             )}
